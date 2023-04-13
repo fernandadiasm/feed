0b72961bcbc8a0142bbe183553c2a6851d0c3039
Fernanda Dias <fernandadiasme@gmail.com>
2023-04-13 17:22:56 -0300

    feat: :sparkles: adiciona um novo comentário

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -16,7 +16,7 @@ export function Comment({ content, onDeleteComment }) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/fernandadiasm.png" />
+      <Avatar hasBorder={false} src="https://camo.githubusercontent.com/5df9498fb809e092756f6cda6b64746eb4f46ddc1b2d85ee894e844cf8da2da6/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f313031353635333830333739313438373039372f313036323736363435343837343834393330312f536d61727453656c6563745f32303232313233315f3232303433375f547769747465722e6a70673f77696474683d353630266865696768743d353632" />
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
