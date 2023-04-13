0d25eb35a0ddcd60ccdeee0d68c6c007916da287
Fernanda Dias <fernandadiasme@gmail.com>
2023-04-13 02:50:18 -0300

    feat: :sparkles: importa estilo e imagem da logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+
+import feedLogo from '../assets/feed-logo.svg';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={feedLogo} alt="Logo do Feed" />
+      <p>Feed</p>
+    </header>
+  );
+}
